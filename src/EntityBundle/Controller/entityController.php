08d82e1b54d8a139fcbb9ca5bf23863e04004a56
Charles <cnerot91@gmail.com>
2017-07-10 00:50:56 +0200

    added a edition and front view

--- src/EntityBundle/Controller/entityController.php
+++ src/EntityBundle/Controller/entityController.php
@@ -18,21 +18,38 @@ class entityController extends Controller
 
     public function viewAction(Request $request)
     {
+
         $em = $this->get('doctrine')->getManager();
         $entity = (new Product())->getById($em, $request->query->get('id', null));
+
+        $data = $request->request->all();
+        if (isset($data['product_id'])){
+            foreach ($data as $key => $val){
+                $key_array = explode('_',$key);
+                if ($key_array['0'] == "attribute"){
+                    $attribute = $entity->data[$key_array[1]];
+                    $attribute->value = $val;
+                    $attribute->save($em);
+                }
+            }
+            $entity = (new Product())->getById($em, $request->query->get('id', null));
+        }
+
+
         return $this->render('EntityBundle:entity:view.html.twig', array(
             'entity' => $entity
-        ));//
+        ));
     }
 
-    public function frontviewAction()
+    public function frontviewAction(Request $request)
     {
         $em = $this->get('doctrine')->getManager();
-        $categories = (new Categories())->getById($em,1);
-        $categories = $categories->save($em);
+        $entity = (new Product())->getById($em, $request->query->get('id', null));
+        $categorie = (new Categories())->getById($em, $entity->id)->name;
 
         return $this->render('EntityBundle:entity:frontview.html.twig', array(
-        //    //"ent" => $entity,
+            'entity' => $entity,
+            'categorie' => $categorie,
         ));
     }
 
