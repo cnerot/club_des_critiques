90653c47ae5ad172fca85dbf103060ee51841a49
sanae <sanai-son112@hotmail.fr>
2017-07-20 14:23:19 +0200

    Statistique

--- src/AppBundle/Controller/StatistiquesController.php
+++ src/AppBundle/Controller/StatistiquesController.php
@@ -7,9 +7,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use AppBundle\Entity\Membre;
+use AppBundle\Entity\Visitecategorie;
 use AppBundle\Entity\Concept;
-
 use EntityBundle\Service\Categories;
+use EntityBundle\Entity\categorie;
+use Symfony\Component\Validator\Constraints\Date;
+
 
 class StatistiquesController extends Controller
 {
@@ -28,19 +31,26 @@ class StatistiquesController extends Controller
         ]);
     }
     /** 
-     * @Route("/noteuser", name="statistiques_noteuser")
+     * @Route("/visiteCategorie", name="statistiques_visiteCategorie")
      */
- /*   public function noteUserAction(Request $request)
+    public function visiteCategorieAction(Request $request)
     {
         $session = $request->getSession();
         $em = $this->getDoctrine()->getManager();
-        $membre = $em->getRepository('AppBundle:Membre')->find($session->get('id'));
+        $visiteCategories = $em->getRepository('AppBundle:Visitecategorie')->findAll();
+        $visites = array();
+        foreach($visiteCategories as $visiteCategorie){
+            $Categories = (new Categories())->getById($em, $visiteCategorie->getId_categorie());
+            $visites[] = array(
+                               $Categories->name,
+                                $visiteCategorie->getNb_visite(),
+                                );
+        }
         
-        return $this->render('administration\statistiques.html.twig', [
-            'id_membre' => $session->get('id'),
-            'membre'=>$membre,
-        ]);
-    }*/
+        return new JsonResponse($visites);
+
+
+    }
     /** 
      * @Route("/articleStat", name="statistiques_articleStat")
      */
@@ -50,13 +60,44 @@ class StatistiquesController extends Controller
         
         $em = $this->get('doctrine')->getManager();
         $categories = (new Categories())->getAll($em);
+        $array = array();
+        foreach ((new Categories())->getAll($em) as $categorie)
+        {
+                $array[] = [
+                         $categorie->name,
+                         count($categorie->getProducts($em)),
+                ];
+        }
         
-        var_dump($categories);
         
-        /*return $this->render('administration\statistiques.html.twig', [
-            'id_membre' => $session->get('id'),
-            'membre'=>$membre,
-        ]);*/
+        
+      
+        return new JsonResponse($array);
+
+    }
+    /** 
+     * @Route("/echangeStat", name="statistiques_echangeStat")
+     */
+    public function echangeStatAction(Request $request)
+    {
+        $session = $request->getSession();
+        
+        $em = $this->get('doctrine')->getManager();
+        $status_own = count($em->getRepository('EntityBundle:Borrow')->findBy(["status"=>7]));
+        $status_share = count($em->getRepository('EntityBundle:Borrow')->findBy(["status"=>1]));
+        $status_interest = count($em->getRepository('EntityBundle:Borrow')->findBy(["status"=>2]));
+        $status_valid = count($em->getRepository('EntityBundle:Borrow')->findBy(["status"=>3]));
+
+
+        $array[] = array("Proprietaire de livre", $status_own + $status_share + $status_interest + $status_valid);
+        $array[] = array("Livre à preter",$status_share + $status_interest + $status_valid);
+        $array[] = array("livre prêté",$status_interest + $status_valid);
+
+        //return $array;
+        
+      
+        return new JsonResponse($array);
+
     }
  
     /** 
@@ -65,44 +106,29 @@ class StatistiquesController extends Controller
     public function nbVisiteAction(Request $request)
     {
         $session = $request->getSession();
-        if(isset($_GET['mois'])){
-            $debut = date_create('2017-0'.$_GET['mois'].'-01');
-            $end = date_create('2017-0'.$_GET['mois'].'-31');
+
             $em = $this->get('doctrine')->getManager();
             $visites = $em->getRepository('AppBundle:Visiteur')->findAll();
             $visiteur = array();
             foreach ($visites as $visite){
-                $date = $visite->getDate_visite();
-                if($date>=$debut && $date<=$end){
                     $datev = $visite->getDate_visite();
-                    $visiteur[] = array(
-                                        $datev->format('Y-m-d'),
-                                        "".$visite->getNb_visite()."",
-                                        );
-                }
+                        if(isset($visiteur[$datev->format('Y-m-d')])){
+                            $visiteur[$datev->format('Y-m-d')] = $visiteur[$datev->format('Y-m-d')]+$visite->getNb_visite();
+                        }else{
+                            $visiteur[$datev->format('Y-m-d')] = $visite->getNb_visite()+1;
+                        }
+                    }
+            $nbVisite=array();
+            foreach($visiteur as $key => $value){
+            $nbVisite[]= array(
+                               $key,"$value"
+                            );
+                
             }
+                   // var_dump($visiteur);
             //echo json_encode($visiteur);
-        }else{
-            $debut = date_create('2017-01-01');
-            $end = date_create('2017-01-31');
-            $em = $this->get('doctrine')->getManager();
-            $visites = $em->getRepository('AppBundle:Visiteur')->findAll();
-            $visiteur = array();
-            foreach ($visites as $visite){
-                $date = $visite->getDate_visite();
-                if($date>=$debut && $date<=$end){
-                    $datev = $visite->getDate_visite();
-                    $visiteur[] = array(
-                                        $datev->format('Y-m-d'),
-                                        $visite->getNb_visite(),
-                                        );
-                }
-            }
-           // var_dump($visiteur["nb_viste"]);
-            //var_dump($visiteur);
-            //echo json_encode($visiteur);
-        }
-        return new JsonResponse($visiteur);
+       
+        return new JsonResponse($nbVisite);
        //return $this->redirectToRoute('statistiques_statistique', []);  
     }
  
--- web/js/dataviz.js
+++ web/js/dataviz.js
@@ -29,42 +29,171 @@ $(document).ready(function(){
               series:[{lineWidth:4, markerOptions:{style:'square'}}]
             });
         }
-       	
-//        function genCategoryAxisRenderer(){
-//            $.jqplot.config.enablePlugins = true;
-//            var s1 = [2, 6, 7, 10];
-//            var ticks = ['a', 'b', 'c', 'd'];
-//
-//            plot1 = $.jqplot('chart1', [s1], {
-//                // Only animate if we're not using excanvas (not in IE 7 or IE 8)..
-//                animate: !$.jqplot.use_excanvas,
-//                seriesDefaults:{
-//                    renderer:$.jqplot.BarRenderer,
-//                    pointLabels: { show: true }
-//                },
-//                axes: {
-//                    xaxis: {
-//                        renderer: $.jqplot.CategoryAxisRenderer,
-//                        ticks: ticks
-//                    }
-//                },
-//                highlighter: { show: false }
-//            });
-//
-//            $('#chart1').bind('jqplotDataClick', 
-//                function (ev, seriesIndex, pointIndex, data) {
-//                    $('#info1').html('series: '+seriesIndex+', point: '+pointIndex+', data: '+data);
-//                }
-//            );
-//        }
-      //  genCategoryAxisRenderer();
-       // generateDateAxis2();
-      /*  getRequest("/nbVisite?mois=7", function(data) {
-                console.log(data);
-                generateDateAxis2('chart3', data);
-	});*/
-        getRequest("/nbVisite?mois=7", function(data) {
+        function generateLineAjax(idDiv, data){
+           console.log(data);
+           Highcharts.chart('container', {
+
+            title: {
+                text: 'Solar Employment Growth by Sector, 2010-2016'
+            },
+
+            subtitle: {
+                text: 'Source: thesolarfoundation.com'
+            },
+
+            yAxis: {
+                title: {
+                    text: 'Nombre de visite'
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle'
+            },
+
+            plotOptions: {
+                series: {
+                    pointStart: 1
+                }
+            },
+
+            series: [{
+                name: 'Visite',
+                data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+            }]
+
+        });
+        }
+        function generate3dpieDonut(idDiv, data){
+            Highcharts.chart(idDiv, {
+            chart: {
+                type: 'pie',
+                options3d: {
+                    enabled: true,
+                    alpha: 45
+                }
+            },
+            title: {
+                text: 'Les categorie les plus visités'
+            },
+            subtitle: {
+                text: '3D donut in Highcharts'
+            },
+            plotOptions: {
+                pie: {
+                    innerSize: 100,
+                    depth: 45
+                }
+            },
+            series: [{
+                name: 'Delivered amount',
+                data: data
+            }]
+        });
+        }
+        function generateColumChart(idDiv ,data){
+            Highcharts.chart(idDiv, {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'Nombre d\'article existant par categorie'
+                },
+                subtitle: {
+                    text: 'Source: <a href="/contenu">contenu</a>'
+                },
+                xAxis: {
+                    type: 'category',
+                    labels: {
+                        rotation: -45,
+                        style: {
+                            fontSize: '13px',
+                            fontFamily: 'Verdana, sans-serif'
+                        }
+                    }
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Population (millions)'
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                tooltip: {
+                    pointFormat: 'Population in 2008: <b>{point.y:.1f} millions</b>'
+                },
+                series: [{
+                    name: 'Population',
+                    data: data,
+                    dataLabels: {
+                        enabled: true,
+                        rotation: -90,
+                        color: '#FFFFFF',
+                        align: 'right',
+                        format: '{point.y:.1f}', // one decimal
+                        y: 10, // 10 pixels down from the top
+                        style: {
+                            fontSize: '13px',
+                            fontFamily: 'Verdana, sans-serif'
+                        }
+                    }
+                }]
+            });
+        }
+        function pyramid(idDiv, data){
+            Highcharts.chart(idDiv, {
+                chart: {
+                    type: 'pyramid'
+                },
+                title: {
+                    text: 'Les livres par statut',
+                    x: -50
+                },
+                plotOptions: {
+                    series: {
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name}</b> ({point.y:,.0f})',
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
+                            softConnector: true
+                        },
+                        center: ['40%', '50%'],
+                        width: '80%'
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                series: [{
+                    name: 'Nombre de livre',
+                    data:data
+                }]
+});
+        }
+
+
+
+
+        generateLineAjax();
+
+        getRequest("/nbVisite", function(data) {
                 console.log(data);
                 generateDateAxis('chart2', data);
+	});
+        getRequest("/articleStat", function(data) {
+                console.log(data);
+                
+                generateColumChart('container', data);
+	});
+        getRequest("/visiteCategorie", function(data) {
+                console.log(data);
+                generate3dpieDonut('container2', data);
+	});
+        getRequest("/echangeStat", function(data) {
+                console.log(data);
+                pyramid('container3', data);
 	});
     });
