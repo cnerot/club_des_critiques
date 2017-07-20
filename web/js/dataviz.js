635e215f58e472181072b06bcffc4c8827c5dc68
Charles <cnerot91@gmail.com>
2017-07-20 16:59:39 +0200

    dataviz

--- web/js/dataviz.js
+++ web/js/dataviz.js
@@ -1,37 +1,38 @@
 //
-$(document).ready(function(){
-       	// Pas de cache sur les requête IMPORTANT !
-	$.ajaxSetup({ cache: false });
-	
-	/*** 
-		On définit ici les fonctions de base qui vont nous servir à la récupération des données
-		Je ne définis que le GET ici, mais il est possible d'utiliser POST pour récupérer ses données (on le verra dans un prochain TP)
-	****/
-	function getRequest(url, callback) {
-		$.get(url, function(data) {
-			//data = $.parseJSON(data);
-			callback(data);
-		});
-	}
-	
-    	/***************************************
-		QUESTION 1 : PIE CHART : - Evolution du popularité
-	****************************************/
-       	function generateDateAxis(idDiv, data) {
-            
-            var plot1 = $.jqplot(idDiv, [data], {
-              title:'Nombre visite par mois',
-              axes:{
-                  xaxis:{
-                      renderer:$.jqplot.DateAxisRenderer
-                  }
-              },
-              series:[{lineWidth:4, markerOptions:{style:'square'}}]
-            });
-        }
-        function generateLineAjax(idDiv, data){
-           console.log(data);
-           Highcharts.chart('container', {
+$(document).ready(function () {
+    // Pas de cache sur les requête IMPORTANT !
+    $.ajaxSetup({cache: false});
+
+    /***
+     On définit ici les fonctions de base qui vont nous servir à la récupération des données
+     Je ne définis que le GET ici, mais il est possible d'utiliser POST pour récupérer ses données (on le verra dans un prochain TP)
+     ****/
+    function getRequest(url, callback) {
+        $.get(url, function (data) {
+            //data = $.parseJSON(data);
+            callback(data);
+        });
+    }
+
+    /***************************************
+     QUESTION 1 : PIE CHART : - Evolution du popularité
+     ****************************************/
+    function generateDateAxis(idDiv, data) {
+
+        var plot1 = $.jqplot(idDiv, [data], {
+            title: 'Nombre visite par mois',
+            axes: {
+                xaxis: {
+                    renderer: $.jqplot.DateAxisRenderer
+                }
+            },
+            series: [{lineWidth: 4, markerOptions: {style: 'square'}}]
+        });
+    }
+
+    function generateLineAjax(idDiv, data) {
+        console.log(data);
+        Highcharts.chart('container', {
 
             title: {
                 text: 'Solar Employment Growth by Sector, 2010-2016'
@@ -64,9 +65,10 @@ $(document).ready(function(){
             }]
 
         });
-        }
-        function generate3dpieDonut(idDiv, data){
-            Highcharts.chart(idDiv, {
+    }
+
+    function generate3dpieDonut(idDiv, data) {
+        Highcharts.chart(idDiv, {
             chart: {
                 type: 'pie',
                 options3d: {
@@ -91,109 +93,181 @@ $(document).ready(function(){
                 data: data
             }]
         });
-        }
-        function generateColumChart(idDiv ,data){
-            Highcharts.chart(idDiv, {
-                chart: {
-                    type: 'column'
-                },
+    }
+
+    function generateColumChart(idDiv, data) {
+        Highcharts.chart(idDiv, {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Article complété par categorie'
+            },
+            subtitle: {
+                text: 'Source: <a href="/contenu">contenu</a>'
+            },
+            xAxis: {
+                type: 'qty de produit',
+                labels: {
+                    rotation: -45,
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            },
+            yAxis: {
+                min: 0,
                 title: {
-                    text: 'Nombre d\'article existant par categorie'
-                },
-                subtitle: {
-                    text: 'Source: <a href="/contenu">contenu</a>'
-                },
-                xAxis: {
-                    type: 'category',
-                    labels: {
-                        rotation: -45,
-                        style: {
-                            fontSize: '13px',
-                            fontFamily: 'Verdana, sans-serif'
-                        }
+                    text: ''
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                pointFormat: ''
+            },
+            series: [{
+                name: 'Population',
+                data: data,
+                dataLabels: {
+                    enabled: true,
+                    rotation: -90,
+                    color: '#FFFFFF',
+                    align: 'right',
+                    format: '{point.y:.1f}', // one decimal
+                    y: 10, // 10 pixels down from the top
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
                     }
-                },
-                yAxis: {
-                    min: 0,
-                    title: {
-                        text: 'Population (millions)'
-                    }
-                },
-                legend: {
-                    enabled: false
-                },
-                tooltip: {
-                    pointFormat: 'Population in 2008: <b>{point.y:.1f} millions</b>'
-                },
-                series: [{
-                    name: 'Population',
-                    data: data,
+                }
+            }]
+        });
+    }
+
+    function pyramid(idDiv, data) {
+        Highcharts.chart(idDiv, {
+            chart: {
+                type: 'pyramid'
+            },
+            title: {
+                text: 'Les livres par statut',
+                x: -50
+            },
+            plotOptions: {
+                series: {
                     dataLabels: {
                         enabled: true,
-                        rotation: -90,
-                        color: '#FFFFFF',
-                        align: 'right',
-                        format: '{point.y:.1f}', // one decimal
-                        y: 10, // 10 pixels down from the top
-                        style: {
-                            fontSize: '13px',
-                            fontFamily: 'Verdana, sans-serif'
-                        }
-                    }
-                }]
-            });
-        }
-        function pyramid(idDiv, data){
-            Highcharts.chart(idDiv, {
-                chart: {
-                    type: 'pyramid'
-                },
+                        format: '<b>{point.name}</b> ({point.y:,.0f})',
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
+                        softConnector: true
+                    },
+                    center: ['40%', '50%'],
+                    width: '80%'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            series: [{
+                name: 'Nombre de livre',
+                data: data
+            }]
+        });
+    }
+
+    function last(idDiv, data) {
+        Highcharts.chart(idDiv, {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Efficiency Optimization by Branch'
+            },
+            xAxis: {
+                categories: [
+                    'Seattle HQ',
+                    'San Francisco',
+                    'Tokyo'
+                ]
+            },
+            yAxis: [{
+                min: 0,
                 title: {
-                    text: 'Les livres par statut',
-                    x: -50
+                    text: 'Employees'
+                }
+            }, {
+                title: {
+                    text: 'Profit (millions)'
                 },
-                plotOptions: {
-                    series: {
-                        dataLabels: {
-                            enabled: true,
-                            format: '<b>{point.name}</b> ({point.y:,.0f})',
-                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
-                            softConnector: true
-                        },
-                        center: ['40%', '50%'],
-                        width: '80%'
-                    }
-                },
-                legend: {
-                    enabled: false
-                },
-                series: [{
-                    name: 'Nombre de livre',
-                    data:data
-                }]
-});
-        }
+                opposite: true
+            }],
+            legend: {
+                shadow: false
+            },
+            tooltip: {
+                shared: true
+            },
+            plotOptions: {
+                column: {
+                    grouping: false,
+                    shadow: false,
+                    borderWidth: 0
+                }
+            },
+            series: data
+        });
+    }
+    function attributes(idDiv, data) {
+        Highcharts.chart(idDiv, {
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Type d\'attribut par categories'
+            },
+            xAxis: {
+                categories: ['Text', 'Select',"date", 'Integer', 'Images']
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Categories'
+                }
+            },
+            legend: {
+                reversed: true
+            },
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            },
+            series: data
+        });
+    }
 
 
+    generateLineAjax();
 
-
-        generateLineAjax();
-
-        getRequest("/nbVisite", function(data) {
-                console.log(data);
-                generateDateAxis('chart2', data);
-	});
-        getRequest("/articleStat", function(data) {
-                console.log(data);
-                
-                generateColumChart('container', data);
-	});
-        getRequest("/visiteCategorie", function(data) {
-                console.log(data);
-                generate3dpieDonut('container2', data);
-	});
-        getRequest("/echangeStat", function(data) {
-                console.log(data);
-                pyramid('container3', data);
-	});
+    getRequest("/nbVisite", function (data) {
+        generateDateAxis('chart2', data);
     });
+    getRequest("/articleStat", function (data) {
+        generateColumChart('container', data);
+    });
+    getRequest("/visiteCategorie", function (data) {
+        generate3dpieDonut('container2', data);
+    });
+    getRequest("/echangeStat", function (data) {
+        pyramid('container3', data);
+    });
+    getRequest("/attributsPerCat", function (data) {
+        attributes('container4', data);
+    });
+    getRequest("/completed", function (data) {
+        last('container5', data);
+    });
+});
